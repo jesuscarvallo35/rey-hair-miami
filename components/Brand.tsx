f3ecf52eceef7100c3754f1d6efb235e55276bb2
jesuscarvallo35 <jesuscarvallo35@gmail.com>
2026-08-04 18:25:45 -0400

    Update Brand.tsx

--- components/Brand.tsx
+++ components/Brand.tsx
@@ -1,11 +1,7 @@
 export default function Brand({ light = false }: { light?: boolean }) {
   return (
     <a className={`brand ${light ? "brand--light" : ""}`} href="#top" aria-label="Rey Hair Miami home">
-      <span className="brand__mark">RH</span>
-      <span className="brand__type">
-        <strong>REY HAIR</strong>
-        <small>MIAMI</small>
-      </span>
+      <img src="/IMG_5410.PNG" alt="Rey Hair Miami" style={{ width: 54, height: 54, borderRadius: "50%", objectFit: "cover", display: "block" }} />
     </a>
   );
 }
