1caa185970755ff029d413bdab9a5786f3551c69
jesuscarvallo35 <jesuscarvallo35@gmail.com>
2026-08-04 18:30:40 -0400

    Update Booking.tsx

--- components/Booking.tsx
+++ components/Booking.tsx
@@ -1,7 +1,7 @@
 export default function Booking() {
   return (
     <section className="booking" id="booking">
-      <div className="shell"><p className="eyebrow">Your appointment awaits</p><h2>Make time for<br /><em>yourself.</em></h2><p>Appointments are booked by text message. Send us a message to request your next visit.</p><a className="button button--dark" href="#location">Text to book <span>↗</span></a></div>
+      <div className="shell"><p className="eyebrow">Your appointment awaits</p><h2>Make time for<br /><em>yourself.</em></h2><p>Appointments are booked by text message. Send us a message to request your next visit.</p><a className="button button--dark" href="sms:+19549342390">Text to book <span>↗</span></a></div>
     </section>
   );
 }
